b63d34f204e31a57dbf64864d4a467441ba3c803
Karns <pkarns@deloitte.com>
2017-03-12 16:47:14 -0400

    testing twitter auth in heroku

--- city_click_code/click.js
+++ city_click_code/click.js
@@ -1,7 +1,4 @@
-//variable for city
-//var city = "Orlando";
-
-	//function displayTrendInfo() {
+//function displayTrendInfo() {
 		
 	var settings = {
   "async": true,
@@ -18,7 +15,7 @@
 //ajax call for settings variable data 
 $.ajax(settings).done(function (response) {
   console.log(response);
-  		for (var i = 0; i < 50; i++) {
+  		for (var i = 0; i < 10; i++) {
   			//storing results from API response in a variable
       	  var results = response[0].trends[i].name;
       	  console.log(results);
@@ -32,41 +29,75 @@ $.ajax(settings).done(function (response) {
         //append each button to the page
         $("#trends").append(newButton);
   		}//response function end
- 		
-
-      	  // for(results) {
-      	  // 	var cityButton = $("<button>");
-      	  // 	cityButton.append(results)
-      	  // }
-
-      	  // //empty #trends div before loop to clear previous trends
-      	  // $("#trends").empty();
-
-      	  // //looping through each result item
-      	  // for (i = 0; i < results.length; i++) {
-      	  // 	console.log(results[i]);
-
-      	  // 	//creating a div for each item
-      	  // 	var trendDiv = $("<div>");
-
-      	  // } //results loop end
-
 }); //ajax end
 
-         
+
+//begin Firebase and Firechat
+//Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4ZMBx8F2AEszwQk4M-GiGaOnjP6QMBsY",
+    authDomain: "firechat-test-fde48.firebaseapp.com",
+    databaseURL: "https://firechat-test-fde48.firebaseio.com",
+    storageBucket: "firechat-test-fde48.appspot.com",
+    messagingSenderId: "423047939499"
+  };
+  firebase.initializeApp(config);
+
+  //create a reference to the database
+var database = firebase.database();
+
+var provider = new firebase.auth.TwitterAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+  // You can use these server side with your app's credentials to access the Twitter API.
+  var token = result.credential.accessToken;
+  var secret = result.credential.secret;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
+
+//begin login to twitter function
+      function login() {
+        // Log the user in via Twitter
+        var provider = new firebase.auth.TwitterAuthProvider();
+        firebase.auth().signInWithPopup(provider).catch(function(error) {
+          console.log("Error authenticating user:", error);
+        });
+      }
+
+      firebase.auth().onAuthStateChanged(function(user) {
+        // Once authenticated, instantiate Firechat with the logged in user
+        if (user) {
+          initChat(user);
+        }
+      });
+
+      function initChat(user) {
+        // Get a Firebase Database ref
+        var chatRef = firebase.database().ref("chat");
+
+        // Create a Firechat instance
+        var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+
+        // Set the Firechat user
+        chat.setUser(user.uid, user.displayName);
+      }         
 
 
 
 
-		// var queryURL = "https://shrouded-ravine-11424.herokuapp.com/1.1/trends/place.json?id=2466256";
-	
-		// //performing Ajax GET request and run function for response after GET is done
-  //     	$.ajax({
-  //     	  url: queryURL,
-  //     	  method: "GET"
-  //     	}).done(function(response) {
-  //     	  console.log(queryURL);
-  //     	});
-	//} //displayTrendInfo function end	
 
    
